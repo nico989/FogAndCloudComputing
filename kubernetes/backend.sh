10e56eaa14a92a86c065ef9b0c28b0bd1f0cfcdc
nicolovinci <vincinicolo98@gmail.com>
2021-05-18 16:32:50 +0200

    Update backend script with error checks

--- kubernetes/backend.sh
+++ kubernetes/backend.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
+ERR=Error.*NotFound
+
 LABEL_SVC="backend"
 SVC=$(kubectl get svc $LABEL_SVC 2>&1 > /dev/null)
-ERR_SVC=Error.*AlreadyExists
 
 LABEL_PVC="storedb"
 PVC=$(kubectl get pvc $LABEL_PVC 2>&1 > /dev/null)
-ERR_PVC=Error.*NotFound
 
-if [[ "$SVC" =~ $ERR_SVC ]];
+if [[ "$SVC" =~ $ERR ]];
 then 
-    echo "SERVICE ALREADY EXISTS"
-else
-    if [[ "$PVC" =~ $ERR_PVC ]];
+    if [[ "$PVC" =~ $ERR ]];
     then
         echo "VOLUME DOES NOT EXIST"
         kubectl create -f volume.yaml
@@ -21,4 +19,6 @@ else
         echo "VOLUME ALREADY EXISTS"
         kubectl create -f service.yaml        
     fi
+else
+    echo "SERVICE ALREADY EXISTS"
 fi
